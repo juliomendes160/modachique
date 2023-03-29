0959263273cde01a5da2655c9ee0051104fddb2c
Julio Mendes <juliomendes160@hotmail.com>
2023-03-29 18:52:30 -0300

    Update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-    $("[data-navegacao] a").click(function(e){
+    $(document).on("click", "[data-pagina] a, [data-navegacao] a", function(e) {
         e.preventDefault();
         $.ajax({url: this.href, success: function(result, status, xhr){
             let parser = new DOMParser();
             let document = parser.parseFromString(result,"text/html");
-            $("title").prop("outerHTML",$(document).find("title").prop("outerHTML"));
-            $("main").prop("outerHTML",$(document).find("main").prop("outerHTML"));
+            $("main").prop("outerHTML", $(document).find("main").prop("outerHTML"));
         }});
     });
 });
