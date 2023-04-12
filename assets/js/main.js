96b07e2d2d10d138f28f32e05117ca499d87d628
Julio Mendes <juliomendes160@hotmail.com>
2023-04-11 22:12:59 -0300

    Update commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,47 @@ import '../../libs/js/jquery-v3.6.4-development.js';
 
 $(document).ready(function(){
 
-    $(document).on("click", "a", Impedir);
+    $(document).on("click", "a", Prevent); // Ouvir: tag <a></a>
 
-    function Impedir(event){
+    function Prevent(event){ // Impedir: carregamento da página
         event.preventDefault();
-        Buscar($(this).attr("href"));
+        Search($(this).attr("href"));
     }
 
-    function Buscar(file){
-        fetch(file)
-        .then(response=>{
-            return response.text();
-        })
-        .then(promise=>{
+    function Search(file){ // Buscar: novo HTML
+        $.ajax({
+            url: file, 
+            success: function(result){
             let dom = new DOMParser();
-            let html = dom.parseFromString(promise,"text/html");
-            document.querySelector("main").outerHTML=html.querySelector("main").outerHTML;
+            let html = dom.parseFromString(result,"text/html");
+            $("main").html($(html).find("main").html());
             history.pushState({},"",file);
-        })
+        }});
     }
 
-    (function(){
+    // function Search(file){ // Buscar: adiciona conteúdo no HTML
+    //     fetch(file)
+    //     .then(response=>{
+    //         return response.text();
+    //     })
+    //     .then(promise=>{
+    //         let dom = new DOMParser();
+    //         let html = dom.parseFromString(promise,"text/html");
+    //         // $("main").replaceWith($(html).find("main"));
+    //         console.log($(html).find("main"));
+    //         document.querySelector("main").outerHTML=html.querySelector("main").outerHTML;
+    //         history.pushState({},"",file);
+    //     })
+    // }
+
+    (function(){ // Redirecionar: página raiz
         if(location.pathname!="/" && location.pathname!="/index.html"){
             localStorage.setItem("page", location.pathname);
             location.href="/";
             return;
         }
-    
         if(localStorage.getItem("page")!=null){
-            Buscar(localStorage.getItem("page"));
+            Search(localStorage.getItem("page"));
             localStorage.clear();
         }
     })()
